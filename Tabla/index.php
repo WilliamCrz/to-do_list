d37ee35110a322257fa49098dfa7a73d95fe1560
WilliamCrz <wcb19082004@gmail.com>
2026-03-21 21:05:34 -0400

    Agregue la cabezera a navbar

--- Tabla/index.php
+++ Tabla/index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <title>Inicio - Gestor de tareas</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <style>
-    body { background-color: #f8f9fa; color: #333; }
-    .tarjeta-custom { background: white; border-radius: 12px; box-shadow: 0 4px 15px rgba(0,0,0,0.03); border: 1px solid #eee; margin-top: 2rem;}
-  </style>
-</head>
-<body>
-
 <?php include("navbar.php"); ?>
 
 <div class="container text-center mt-5">
--- Tabla/navbar.php
+++ Tabla/navbar.php
@@ -1,16 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Inicio - Gestor de tareas</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <style>
+    body { background-color: #f8f9fa; color: #333; }
+    .tarjeta-custom { background: white; border-radius: 12px; box-shadow: 0 4px 15px rgba(0,0,0,0.03); border: 1px solid #eee; margin-top: 2rem;}
+  </style>
+</head>
+<body>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-5">
   <div class="container">
-    <a class="navbar-brand" href="index.php">To Do List</a>
+    <a class="navbar-brand" href="../index.php">To Do List</a>
 
     <ul class="navbar-nav ms-auto">
       <li class="nav-item">
-        <a class="nav-link" href="index.php">Inicio</a>
+        <a class="nav-link" href="../Tabla/index.php">Inicio</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="tareas.php">Mis tareas</a>
+        <a class="nav-link" href="../Tabla/mi_tareas.php">Mis tareas</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="crear.php">Nueva tarea</a>
+        <a class="nav-link" href="../Tabla/nueva_tarea.php">Nueva tarea</a>
       </li>
     </ul>
   </div>
